0322a026c0744e99412f1c6a59177a2b61291787
Ben Belland <benbelland@gmail.com>
2014-01-05 16:07:09 -0700

    initial setup and tooling

---
+++ mining-manager.js
@@ -0,0 +1,14 @@
+
+
+function mining_manager()
+{
+	var gameCanvas = document.getElementById('canvas');
+	var context = gameCanvas.getContext('2d');
+	context.fillStyle = 'red';
+	context.fillRect(30, 30, 50, 50);
+}
+ $(document).ready(function(){
+     mining_manager();
+	 
+    });
+ 2 + 2
