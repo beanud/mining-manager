2af896bf4a119b9f69dab0292a7eb8568c68a24a
Ben Belland <benbelland@gmail.com>
2014-01-05 18:20:31 -0700

    mapping updates

--- mining-manager.js
+++ mining-manager.js
@@ -1,12 +1,28 @@
 TILEWIDTH = 64
 
+x = 0;
+mapx =
+	[16,16,16,16,16,16,
+	 16,16,16,16,16,16,
+	 16,16,16,16,16,16,
+	 16,16,16,16,16,16,
+	 16,16,16,16,16,16,
+	 16,16,16,16,16,16]
+mapy = 
+	[192,0,0,0,0,0,
+	 0,192,0,0,0,0,
+	 0,0,192,0,0,0,
+	 0,0,0,192,0,0,
+	 0,0,0,0,192,0,
+	 0,0,0,0,0,192]
+
 function init() {
 
 	ctx = $('#canvas')[0].getContext("2d");
 	WIDTH = $("#canvas").width();
 	HEIGHT = $("#canvas").height();
-	NCOLS = WIDTH / TILEWIDTH;
-	NROWS = HEIGHT / TILEWIDTH;
+	NCOLS = mapx.length / 6;
+	NROWS = mapy.length / 6;
 	canvasMinX = $("#canvas").offset().left;
 	canvasMaxX = canvasMinX + WIDTH
 	intervalId =  setInterval(draw, 10);
@@ -17,15 +33,23 @@ function init() {
 
 function drawBackground()
 {
+	row = 0;
+	col = 0;
 	for (i = 0;i < NCOLS;i++)
 	{
+		if (i >= 1)
+		{
+			col += 1;
+		}
 		//console.log(tilex);
 		for (j = 0;j < NROWS;j++)
 		{
-			
-			ctx.fillStyle = 'red';
+			bckx = mapx[col];
+			bcky = mapy[row];			
+			//ctx.fillStyle = 'red';
 			//ctx.fillRect(tilex + 200, 200, 10, 10);
-			ctx.drawImage(document.getElementById('background'), 0, 0, 32, 32, TILEWIDTH * i, TILEWIDTH * j, TILEWIDTH, TILEWIDTH);
+			ctx.drawImage(document.getElementById('background'), bckx, bcky, 16, 16, TILEWIDTH * i, TILEWIDTH * j, TILEWIDTH, TILEWIDTH);
+			row += 1;
 		}
 	}
 	
