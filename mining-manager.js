196b08a5d363653126669e79efaf4057b85309a4
Ben Belland <benbelland@gmail.com>
2014-01-05 17:15:58 -0700

    background drawing

--- mining-manager.js
+++ mining-manager.js
@@ -5,27 +5,27 @@ function init() {
 	ctx = $('#canvas')[0].getContext("2d");
 	WIDTH = $("#canvas").width();
 	HEIGHT = $("#canvas").height();
-	NCOLS = TILEWIDTH / WIDTH;
-	NROWS = TILEWIDTH / HEIGHT;
+	NCOLS = WIDTH / TILEWIDTH;
+	NROWS = HEIGHT / TILEWIDTH;
 	canvasMinX = $("#canvas").offset().left;
 	canvasMaxX = canvasMinX + WIDTH
 	intervalId =  setInterval(draw, 10);
 	console.log(TILEWIDTH);
+	console.log(NCOLS);
+	console.log(NROWS);
     }
 
 function drawBackground()
 {
 	for (i = 0;i < NCOLS;i++)
 	{
-		tilex = TILEWIDTH * i;
 		//console.log(tilex);
 		for (j = 0;j < NROWS;j++)
 		{
 			
-			tiley = TILEWIDTH * j;
 			ctx.fillStyle = 'red';
-			ctx.fillRect(tilex + 200, tiley + 200, 10, 10);
-			ctx.drawImage(document.getElementById('background'), 0, 0, 32, 32, tilex, tiley, TILEWIDTH, TILEWIDTH);
+			//ctx.fillRect(tilex + 200, 200, 10, 10);
+			ctx.drawImage(document.getElementById('background'), 0, 0, 32, 32, TILEWIDTH * i, TILEWIDTH * j, TILEWIDTH, TILEWIDTH);
 		}
 	}
 	
